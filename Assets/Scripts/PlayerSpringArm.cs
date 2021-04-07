5394da8408bc7030ac41e8cab65979ca6764fc69
DemonKing252 <46793138+DemonKing252@users.noreply.github.com>
2021-04-06 23:47:38 -0400

    Added functional spring arm third person camera

--- Assets/Scripts/PlayerSpringArm.cs
+++ Assets/Scripts/PlayerSpringArm.cs
@@ -5,19 +5,42 @@ using UnityEngine;
 public class PlayerSpringArm : MonoBehaviour
 {
     [SerializeField]
+    private float armHeight = 1f;   // height from centre of player
+
+    [SerializeField]
     private float armLength = 0f;
 
+    [SerializeField]
+    private float dampingCoeff = 1f;
+
+    [SerializeField]
+    private float springConstant = -200f;
+
+    // Damping
+    private Vector3 previousVel;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = GameObject.FindGameObjectWithTag("Player").transform.position - new Vector3(0f, -1f, armLength);
+        // Spring arm starts at rest position:
+        transform.position = GameObject.FindGameObjectWithTag("Player").transform.position - new Vector3(0f, -armHeight, armLength);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Vector3 origin = GameObject.FindGameObjectWithTag("Player").transform.position - new Vector3(0f, -armHeight, armLength);
+        Vector3 direction = origin - transform.position;
+
+        float mag = Vector3.Distance(transform.position, origin);
+
+        // Spring force = -coeff of friction x distance from origin of spring
+        // Fs = -k * x
+        Vector3 normalForce = -springConstant * direction - dampingCoeff * (GetComponent<Rigidbody>().velocity - previousVel);
+        GetComponent<Rigidbody>().AddForce(normalForce, ForceMode.Acceleration);
+        previousVel = GetComponent<Rigidbody>().velocity;
+
+
         // Need spring here
-        transform.position = GameObject.FindGameObjectWithTag("Player").transform.position - new Vector3(0f, -1f, armLength);
     }
 }
